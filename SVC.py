8bcc9719e7993badeaf375f53231daf884b95330
alexey <sav-alexey@mail.ru>
2018-11-05 20:46:37 +0700

    added plots

--- SVC.py
+++ SVC.py
@@ -3,66 +3,103 @@ import pandas as pd
 from sklearn.metrics import accuracy_score
 from sklearn.svm import SVC
 from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
 import datetime 
+import matplotlib.pyplot as plt
 
 pandas_train = pd.read_csv("train.csv")
 pandas_train = pandas_train.drop(["Id"], axis=1)
 columns = pandas_train.axes[1]
 data_train = np.array(pandas_train)
-X_train = data_train[:,:-1]
-Y_train = data_train[:,-1]
+X = data_train[:,:-1]
+Y = data_train[:,-1]
 #Y_train = Y_train.reshape([Y_train.shape[0], 1])
+X_train, X_valid, Y_train, Y_valid = train_test_split(X, Y, test_size=0.2, random_state=42)
+#pandas_test = pd.read_csv("train.csv")
+#pandas_test = pandas_test.drop(["Id"], axis=1)
+#data_test = np.array(pandas_test)
+#X_test = data_test[:,:-1]
 
-pandas_test = pd.read_csv("train.csv")
-pandas_test = pandas_test.drop(["Id"], axis=1)
-data_test = np.array(pandas_test)
-X_test = data_test[:,:-1]
 
-
-print("SVC without preprocessing:")
-start_time = datetime.datetime.now()
-clf = SVC(C=1, kernel='rbf')
-clf.fit(X_train, Y_train)
-print("SVC time execution(kernel='rbf' and C=1 ):",
-      datetime.datetime.now() - start_time)
-
-prediction = clf.predict(X_test)
-acc_score = accuracy_score(Y_train, prediction)
-print("Accuracy score = {0}".format(acc_score))
-
+#print("SVC without preprocessing:")
+#start_time = datetime.datetime.now()
+#clf = SVC(C=1, kernel='rbf')
+#clf.fit(X_train, Y_train)
+#print("SVC time execution(kernel='rbf' and C=1 ):",
+#      datetime.datetime.now() - start_time)
+#
+#prediction = clf.predict(X_train)
+#acc_score_train = accuracy_score(Y_train, prediction)
+#prediction = clf.predict(X_valid)
+#acc_score_valid = accuracy_score(Y_valid, prediction)
+#print("Train accuracy = {0}, Test accuracy = {1}".format(acc_score_train, acc_score_valid))
+#
+##Normalizing
+#X_train = preprocessing.normalize(X_train, norm='l2')
+#X_valid = preprocessing.normalize(X_valid, norm='l2')
+#
+#print("\nSVC with normalizing:")
+#start_time = datetime.datetime.now()
+#clf = SVC(C=1, kernel='rbf')
+#clf.fit(X_train, Y_train)
+#print("SVC time execution(kernel='rbf' and C=1 ):",
+#      datetime.datetime.now() - start_time)
+#
+#prediction = clf.predict(X_train)
+#acc_score_train = accuracy_score(Y_train, prediction)
+#prediction = clf.predict(X_valid)
+#acc_score_valid = accuracy_score(Y_valid, prediction)
+#print("Train accuracy = {0}, Test accuracy = {1}".format(acc_score_train, acc_score_valid))
+#
 # Scaling
 X_train = preprocessing.scale(X_train)
-X_test = preprocessing.scale(X_test)
+X_valid = preprocessing.scale(X_valid)
 
-print("\nSVC with scaling:")
-start_time = datetime.datetime.now()
-clf = SVC(C=1, kernel='rbf')
-clf.fit(X_train, Y_train)
-print("SVC time execution(kernel='rbf' and C=1 ):",
-      datetime.datetime.now() - start_time)
+#print("\nSVC with scaling:")
+#start_time = datetime.datetime.now()
+#clf = SVC(C=1, kernel='rbf')
+#clf.fit(X_train, Y_train)
+#print("SVC time execution(kernel='rbf' and C=1 ):",
+#      datetime.datetime.now() - start_time)
+#
+#prediction = clf.predict(X_train)
+#acc_score_train = accuracy_score(Y_train, prediction)
+#prediction = clf.predict(X_valid)
+#acc_score_valid = accuracy_score(Y_valid, prediction)
+#print("Train accuracy = {0}, Test accuracy = {1}".format(acc_score_train, acc_score_valid))
 
-prediction = clf.predict(X_test)
-acc_score = accuracy_score(Y_train, prediction)
-print("Accuracy score = {0}".format(acc_score))
-
-#Normalizing
-X_train = preprocessing.normalize(X_train, norm='l2')
-X_test = preprocessing.normalize(X_test, norm='l2')
-
-print("\nSVC with normalizing:")
-start_time = datetime.datetime.now()
-clf = SVC(C=1, kernel='rbf')
-clf.fit(X_train, Y_train)
-print("SVC time execution(kernel='rbf' and C=1 ):",
-      datetime.datetime.now() - start_time)
-
-prediction = clf.predict(X_test)
-acc_score = accuracy_score(Y_train, prediction)
-print("Accuracy score = {0}".format(acc_score))
+acc_train, acc_valid = [], []
+power = [i for i in range(-6, 6)]
+for i in power:
+    start_time = datetime.datetime.now()
+    clf = SVC(C=10**i, kernel='rbf')
+    clf.fit(X_train, Y_train)
+    print("SVC time execution(kernel='rbf' and C={0}):".format(10**i),
+          datetime.datetime.now() - start_time)
+    
+    prediction = clf.predict(X_train)
+    acc_score_train = accuracy_score(Y_train, prediction)
+    acc_train.append(acc_score_train)
+    prediction = clf.predict(X_valid)
+    acc_score_valid = accuracy_score(Y_valid, prediction)
+    acc_valid.append(acc_score_valid)
+    print("Train accuracy = {0}, Test accuracy = {1}".format(acc_score_train, acc_score_valid))
+    
+plt.plot(power, acc_train)
+plt.plot(power, acc_valid)
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('C^x')
+plt.legend(['train', 'valid'], loc='upper left')
+plt.show()
 
 '''
 Results:
-Without preprocessing SVC time execution = 45s and accuracy = 1
-With scaling SVC time execution = 6s but accuracy = 0.75
-With normalization  SVC time execution = 10s accuracy = 0.61
+Without preprocessing SVC time execution = 45s
+Train accuracy = 1.0, Test accuracy = 0.1402116402116402
+With normalization  SVC time execution = 10s
+Train accuracy = 0.6130125661375662, Test accuracy = 0.6233465608465608
+With scaling SVC time execution = 6s
+Train accuracy = 0.7405753968253969, Test accuracy = 0.7377645502645502
+
 ''' 
--- decision_tree.py
+++ decision_tree.py
@@ -2,39 +2,69 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics import accuracy_score
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
 import datetime 
+import matplotlib.pyplot as plt
 
 pandas_train = pd.read_csv("train.csv")
-pandas_train = pandas_train.drop(["Id"], axis=1)
+X_pandas = pandas_train.drop(["Cover_Type", "Id"], axis=1)
 columns = pandas_train.axes[1]
-data_train = np.array(pandas_train)
-X_train = data_train[:,:-1]
-Y_train = data_train[:,-1]
-Y_train = Y_train.reshape([Y_train.shape[0], 1])
+X = np.array(X_pandas)
+Y = np.array(pandas_train)[:,-1]
+Y = Y.reshape([Y.shape[0], 1])
 
-pandas_test = pd.read_csv("train.csv")
-pandas_test = pandas_test.drop(["Id"], axis=1)
-data_test = np.array(pandas_test)
-X_test = data_test[:,:-1]
+#pandas_test = pd.read_csv("test.csv")
+#pandas_test = pandas_test.drop(["Id"], axis=1)
+#X_test = np.array(pandas_test)
 
+X_train, X_valid, Y_train, Y_valid = train_test_split(X, Y, test_size=0.2, random_state=42)
+
+acc_train, acc_valid = [], []
 print("Choosing the best depth for criterion='entropy':")
 for i in range(3, 35):  
     clf = DecisionTreeClassifier(criterion='entropy', max_depth=i, random_state=1)
     clf.fit(X_train, Y_train)
-    prediction = clf.predict(X_test)
-    acc_score = accuracy_score(Y_train, prediction)
-    print("Depth = {0}, Accuracy = {1}".format(i, acc_score))
-print("The best depth is 25")
-    
+    prediction = clf.predict(X_train)
+    acc_score_train = accuracy_score(Y_train, prediction)
+    acc_train.append(acc_score_train)
+    prediction = clf.predict(X_valid)
+    acc_score_valid = accuracy_score(Y_valid, prediction)
+    acc_valid.append(acc_score_valid)
+    print("""Depth = {0}, Train accuracy = {1},
+          Valid accuracy = {2}""".format(i, acc_score_train, acc_score_valid))
+print("The best depth is 26")
+
+plt.plot(range(3, 35), acc_train)
+plt.plot(range(3, 35), acc_valid)
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('depth')
+plt.legend(['train', 'valid'], loc='upper left')
+plt.show()
+
+acc_train, acc_valid = [], []    
 print("\nChoosing the best depth for criterion='gini':")
 for i in range(3, 35):  
     clf = DecisionTreeClassifier(criterion='gini', max_depth=i, random_state=1)
     clf.fit(X_train, Y_train)
-    prediction = clf.predict(X_test)
-    acc_score = accuracy_score(Y_train, prediction)
-    print("Depth = {0}, Accuracy = {1}".format(i, acc_score))   
-print("The best depth is 32")    
-    
+    prediction = clf.predict(X_train)
+    acc_score_train = accuracy_score(Y_train, prediction)
+    acc_train.append(acc_score_train)
+    prediction = clf.predict(X_valid)
+    acc_score_valid = accuracy_score(Y_valid, prediction)
+    acc_valid.append(acc_score_valid)
+    print("""Depth = {0}, Train accuracy = {1}",
+          Valid accuracy = {2}""".format(i, acc_score_train, acc_score_valid)) 
+print("The best depth is 31")    
+
+plt.plot(range(3, 35), acc_train)
+plt.plot(range(3, 35), acc_valid)
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('depth')
+plt.legend(['train', 'valid'], loc='upper left')
+plt.show()
+  
 # Extracting not important features
 importances = clf.feature_importances_
 zero_imp_columns = []
@@ -48,24 +78,36 @@ for i, name in zip(importances, columns):
 for f in zero_imp_columns:
     pandas_train = pandas_train.drop([f], axis=1)
 
-# Deleting not important features from test data
-for f in zero_imp_columns:
-    pandas_test = pandas_test.drop([f], axis=1)
+X_pandas = pandas_train.drop(["Cover_Type", "Id"], axis=1)
+columns = pandas_train.axes[1]
+X = np.array(X_pandas)
+Y = np.array(pandas_train)[:,-1]
+Y = Y.reshape([Y.shape[0], 1])
+X_train, X_valid, Y_train, Y_valid = train_test_split(X, Y, test_size=0.2, random_state=42)
+## Deleting not important features from test data
+#for f in zero_imp_columns:
+#    pandas_test = pandas_test.drop([f], axis=1)
  
 # Time execution for criterion='entropy' and depth=25 
 start_time = datetime.datetime.now()
-clf = DecisionTreeClassifier(criterion='entropy', max_depth=25, random_state=1)
+clf = DecisionTreeClassifier(criterion='entropy', max_depth=26, random_state=1)
 clf.fit(X_train, Y_train)
-print("\nDecision tree time execution(criterion='entropy' and depth=25 ):",
-      datetime.datetime.now() - start_time)
+prediction = clf.predict(X_train)
+acc_score_train = accuracy_score(Y_train, prediction)
+prediction = clf.predict(X_valid)
+acc_score_valid = accuracy_score(Y_valid, prediction)
+print("""\nDecision tree time execution(criterion='entropy' and depth=26 ): {0},
+      Train accuracy = {1},
+      Valid accuracy = {2}""".format(datetime.datetime.now() - start_time, acc_score_train, acc_score_valid))
 
 # Time execution for criterion='gini' and depth=32 
 start_time = datetime.datetime.now()
-clf = DecisionTreeClassifier(criterion='gini', max_depth=32, random_state=1)
+clf = DecisionTreeClassifier(criterion='gini', max_depth=31, random_state=1)
 clf.fit(X_train, Y_train)
-print("Decision tree time execution(criterion='gini' and depth=32 ):",
-      datetime.datetime.now() - start_time)
-
+print("""\nDecision tree time execution(criterion='gini' and depth=31 ): {0},
+      Train accuracy = {1},
+      Valid accuracy = {2}""".format(datetime.datetime.now() - start_time, acc_score_train, acc_score_valid))
+     
 '''
 Results:
 the best depth for criterion='entropy' = 25
@@ -73,9 +115,4 @@ Decision tree time execution(criterion='entropy' and depth=25 ): 0:00:00.177472
 the best depth for criterion='gini' = 32
 Decision tree time execution(criterion='gini' and depth=32 ): 0:00:00.142379
 ''' 
-
-
-
-
-
 
